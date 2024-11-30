108758670e7a367d579b90801a743d24e7a1e5ae
Saruf Ratul <43202031+Saruf-Ratul@users.noreply.github.com>
2024-11-30 11:37:23 +0600

    UI

--- xsi-franchisor.client/src/app/app.component.ts
+++ xsi-franchisor.client/src/app/app.component.ts
@@ -33,5 +33,5 @@ export class AppComponent implements OnInit {
     );
   }
 
-  title = 'xsi-franchisor.client';
+  title = 'XSI-Franchisor';
 }
--- xsi-franchisor.client/src/app/app.module.ts
+++ xsi-franchisor.client/src/app/app.module.ts
@@ -4,14 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
